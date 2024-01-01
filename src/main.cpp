70ba5be63319c59088b607c83d43f8330167f590
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-01-01 15:24:14 +0100

    remove delays. make code more readable

--- src/main.cpp
+++ src/main.cpp
@@ -7,32 +7,41 @@ const int firstBtnPin = 13;
 const int secondBtnPin = 10;
 const int thirdBtnPin = 9;
 
-void setup() {
-    Serial.begin(9600); 
-    lcd.begin(16, 2);
-    lcd.print("time");
-
-
+void printMessage(String message) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Buon anno!");
+    delay(5000);
 }
 
-void loop() {
-    if(digitalRead(secondBtnPin) == HIGH) {
-        lcd.clear();
-    }
-
+void retrieveData() {
     if (Serial.available() > 0) {
         lcd.clear();
         String timestampString = Serial.readStringUntil('\n');
         const int splitter = timestampString.indexOf(' ');
         
-        const String date = timestampString.substring(0, splitter);
+        String date = timestampString.substring(0, splitter);
         lcd.setCursor(0, 0);
         lcd.print(date);
 
-        const String time = timestampString.substring(splitter+3);
+        String time = timestampString.substring(splitter+3);
         lcd.setCursor(0, 1);
         lcd.print(time);
     }
+}
 
-    delay(1000);
+void setup() {
+    Serial.begin(9600); 
+    lcd.begin(16, 2);
+    lcd.print("time");
+}
+
+void loop() {
+    if(digitalRead(firstBtnPin) == HIGH) {
+        Serial.end();
+        printMessage("Buon anno");
+        Serial.begin(9600); 
+    }
+
+    retrieveData();
 }
