e4aebeb73b46973c3121e5b24d1124c1ca6c7cf7
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-01-07 16:42:37 +0100

    print weather data when button is pressed

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,92 @@
 #include <Arduino.h>
-#include <LiquidCrystal.h> 
+#include <LiquidCrystal.h>
+#include <ezButton.h>
+#include <stdio.h>
+#include <string.h>
+#include <arduino-timer.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
+#define FIRST_BUTTON 13
 
-const int firstBtnPin = 13;
-const int secondBtnPin = 10;
-const int thirdBtnPin = 9;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);   
+ezButton button(FIRST_BUTTON);  
 
-void printMessage(String message) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Buon anno!");
-    delay(5000);
+bool Arduino_isGettingData;
+auto timer = timer_create_default();
+auto weatherUpdater = timer_create_default();
+
+String currentWeather;
+
+void CPPCOMM(String message) {
+    char *to_send = &message[0];
+    Serial.write(to_send);
 }
 
-void retrieveData() {
+void LCDWrite(String text, int line, bool clear = false) {
+    if(clear == true) {
+        lcd.clear();
+    }
+
+    lcd.setCursor(0, line);
+    lcd.print(text);
+}
+
+bool refreshWeater(void *) {
+    currentWeather = true;
+    return true;
+}
+
+bool retrieveData(void *) {
+    CPPCOMM("<time>");
+
     if (Serial.available() > 0) {
         lcd.clear();
         String timestampString = Serial.readStringUntil('\n');
         const int splitter = timestampString.indexOf(' ');
-        
+            
         String date = timestampString.substring(0, splitter);
-        lcd.setCursor(0, 0);
-        lcd.print(date);
+        LCDWrite(date, 0);
 
         String time = timestampString.substring(splitter+1);
-        lcd.setCursor(0, 1);
-        lcd.print(time);
+        LCDWrite(time, 1);
     }
+
+    return true;
 }
 
 void setup() {
-    pinMode(firstBtnPin, INPUT);
+    Arduino_isGettingData = true;
+
+    button.setDebounceTime(1);
+
     Serial.begin(9600); 
     lcd.begin(16, 2);
     lcd.print("waiting for data...");
+    timer.every(1000, retrieveData);
+    weatherUpdater.every(600000, refreshWeater);
 }
 
 void loop() {
-    if(digitalRead(firstBtnPin) == HIGH) {
-        String message = "< scemo >";
-        char *to_send = &message[0];
-        Serial.println(to_send);
+    button.loop();
+
+    if(Arduino_isGettingData == true) {
+        CPPCOMM("<weather>");
+        delay(5000);
+
+        if(Serial.available()) {
+            currentWeather = Serial.readString();
+            Arduino_isGettingData = false;
+        }
     }
 
-    retrieveData();
+    if(Arduino_isGettingData == false) {
+        timer.tick();
+        weatherUpdater.tick();
+    }
+
+    if(button.isPressed()) {
+        Serial.end();
+        LCDWrite(currentWeather, 0, true);
+        delay(5000);
+        Serial.begin(9600); 
+    }
 }
